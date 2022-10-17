f2a1e6cf07e9cda596c3e5fa2540bec4f0bf2aec
RyanfanyfadlyllahAR <fadlyllahfanny@gmail.com>
2022-10-17 20:45:51 +0700

    vidio 3

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('home');
 });
+
+Route::get('/about', function () {
+    return view ('about', [
+        "name" => "Ryan fany fadlyllah AR",
+        "email" => "fadlyllahfanny@gmail.com",
+        "image" => "fadlyllah.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view ('posts');
+});
+
+
