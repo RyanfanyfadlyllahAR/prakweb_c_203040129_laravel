91c0dd2b4b0f7002acb6d70662d4531d8fd8210b
RyanfanyfadlyllahAR <fadlyllahfanny@gmail.com>
2022-10-24 11:35:06 +0700

    menyelesaikan vidio4

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,68 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view ('home');
+    return view ('home', [
+    "title"=> "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view ('about', [
+        "title" => "About",
         "name" => "Ryan fany fadlyllah AR",
         "email" => "fadlyllahfanny@gmail.com",
         "image" => "fadlyllah.jpg"
     ]);
 });
 
+
+
 Route::get('/blog', function () {
-    return view ('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul post pertama",
+            "slug" => "Judul-Post-Pertama",
+            "author" => "Ryan fany fadlyllah Ar",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius quae totam magni repellendus error, laborum, ipsam nesciunt sunt omnis facere non. Ut recusandae et qui eos fugit. Ex mollitia id perferendis amet sint vel animi ea porro repellendus. Optio illo praesentium iure aspernatur necessitatibus non recusandae, dolores blanditiis a, excepturi eum modi molestias commodi est. Corporis, commodi quibusdam perspiciatis sit repudiandae necessitatibus voluptatum asperiores nemo nostrum. Unde temporibus quisquam eaque soluta. Rerum animi labore esse consequatur ad, corrupti ducimus praesentium?"
+        ],
+        [
+            "title" => "Judul post kudua",
+            "slug"  => "Judul-Post-Kedua",
+            "author" => "fadlyllah",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius quae totam magni repellendus error, laborum, ipsam nesciunt sunt omnis facere non. Ut recusandae et qui eos fugit. Ex mollitia id perferendis amet sint vel animi ea porro repellendus. Optio illo praesentium iure aspernatur necessitatibus non recusandae, dolores blanditiis a, excepturi eum modi molestias commodi est. Corporis, commodi quibusdam perspiciatis sit repudiandae necessitatibus voluptatum asperiores nemo nostrum. Unde temporibus quisquam eaque soluta. Rerum animi labore esse consequatur ad, corrupti ducimus praesentium?"
+        ],
+    ];
+    return view ('posts', [
+        "title" => "Posts",
+        "post" => $blog_posts
+    ]);
 });
 
+// halaman single post
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul post pertama",
+            "slug" => "Judul-Post-Pertama",
+            "author" => "Ryan fany fadlyllah Ar",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius quae totam magni repellendus error, laborum, ipsam nesciunt sunt omnis facere non. Ut recusandae et qui eos fugit. Ex mollitia id perferendis amet sint vel animi ea porro repellendus. Optio illo praesentium iure aspernatur necessitatibus non recusandae, dolores blanditiis a, excepturi eum modi molestias commodi est. Corporis, commodi quibusdam perspiciatis sit repudiandae necessitatibus voluptatum asperiores nemo nostrum. Unde temporibus quisquam eaque soluta. Rerum animi labore esse consequatur ad, corrupti ducimus praesentium?"
+        ],
+        [
+            "title" => "Judul post kudua",
+            "slug"  => "Judul-Post-Kedua",
+            "author" => "fadlyllah",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius quae totam magni repellendus error, laborum, ipsam nesciunt sunt omnis facere non. Ut recusandae et qui eos fugit. Ex mollitia id perferendis amet sint vel animi ea porro repellendus. Optio illo praesentium iure aspernatur necessitatibus non recusandae, dolores blanditiis a, excepturi eum modi molestias commodi est. Corporis, commodi quibusdam perspiciatis sit repudiandae necessitatibus voluptatum asperiores nemo nostrum. Unde temporibus quisquam eaque soluta. Rerum animi labore esse consequatur ad, corrupti ducimus praesentium?"
+        ],
+    ];
 
+    $new_post =[];
+    foreach($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
+});
