c5a4b22f6012e0830db4b1f5c4f21907eb57c0ed
RyanfanyfadlyllahAR <fadlyllahfanny@gmail.com>
2022-10-26 23:52:09 +0700

    vidio7 dan 8

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,8 @@
 @section('container')
 <h1 class="mb-5">{{ $post->title }}</h1>
 
+<p>By.Ryan fany fadlyllah Ar in <a href="/categories/">{{$post->category->name}}</a> </p>
+
 {!! $post->body !!}
 
 <a href="/posts">Back to Posts</a>
