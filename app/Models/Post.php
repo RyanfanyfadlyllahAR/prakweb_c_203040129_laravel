40025e0fbc326c7484a5a2b89914755b466d327f
RyanfanyfadlyllahAR <fadlyllahfanny@gmail.com>
2022-10-26 13:11:07 +0700

    vidio6 laravel

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,28 +1,11 @@
 <?php
 
-namespace app\Models;
+namespace App\Models;
 
-class Post  {
-    private static $blog_posts = [
-        [
-            "title" => "Judul post pertama",
-            "slug" => "Judul-Post-Pertama",
-            "author" => "Ryan fany fadlyllah Ar",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius quae totam magni repellendus error, laborum, ipsam nesciunt sunt omnis facere non. Ut recusandae et qui eos fugit. Ex mollitia id perferendis amet sint vel animi ea porro repellendus. Optio illo praesentium iure aspernatur necessitatibus non recusandae, dolores blanditiis a, excepturi eum modi molestias commodi est. Corporis, commodi quibusdam perspiciatis sit repudiandae necessitatibus voluptatum asperiores nemo nostrum. Unde temporibus quisquam eaque soluta. Rerum animi labore esse consequatur ad, corrupti ducimus praesentium?"
-        ],
-        [
-            "title" => "Judul post kudua ",
-            "slug"  => "Judul-Post-Kedua",
-            "author" => "fadlyllah",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius quae totam magni repellendus error, laborum, ipsam nesciunt sunt omnis facere non. Ut recusandae et qui eos fugit. Ex mollitia id perferendis amet sint vel animi ea porro repellendus. Optio illo praesentium iure aspernatur necessitatibus non recusandae, dolores blanditiis a, excepturi eum modi molestias commodi est. Corporis, commodi quibusdam perspiciatis sit repudiandae necessitatibus voluptatum asperiores nemo nostrum. Unde temporibus quisquam eaque soluta. Rerum animi labore esse consequatur ad, corrupti ducimus praesentium?"
-        ]
-    ];
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function all(){
-        return collect(self::$blog_posts);
-    }
-    public static function find($slug){
-        $posts = static::all();
-      return $posts->firstWhere('slug',$slug);
-  }
+class Post extends Model
+{
+    use HasFactory;
 }
