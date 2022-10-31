9e2d1646a6a596408e2675c75f2584c164bed6bf
RyanfanyfadlyllahAR <fadlyllahfanny@gmail.com>
2022-10-31 17:01:31 +0700

    menyelesaikan vidio 8

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
 @section('container')
 <h1 class="mb-5">{{ $post->title }}</h1>
 
-<p>By.Ryan fany fadlyllah Ar in <a href="/categories/">{{$post->category->name}}</a> </p>
+<p>By.Ryan fany fadlyllah Ar in <a href="/categories/{{ $post->category->slug }}">{{$post->category->name}}</a></p>
 
 {!! $post->body !!}
 
