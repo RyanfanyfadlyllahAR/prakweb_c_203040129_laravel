179284473acd23416e417f28e9f298ee4d871cbc
RyanfanyfadlyllahAR <fadlyllahfanny@gmail.com>
2022-10-31 21:07:38 +0700

    vidio 9

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\User;
+    use App\Models\Category;
+    use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class DatabaseSeeder extends Seeder 
 {
     /**
      * Seed the application's database.
@@ -13,6 +16,62 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Ryan fany fadlyllah Ar',
+            'email' => 'fadlyllahfanny@gmail.com',
+            'password' => bcrypt(12345)
+        ]);
+        User::create([
+            'name' => 'fadlyllah Ar',
+            'email' => 'fadlyllah@gmail.com',
+            'password' => bcrypt(12345)
+        ]);
+
+        Category::create([
+            'name'=> 'Web programing',
+            'slug'=> 'web-programing'
+        ]);
+
+        Category::create([
+            'name'=> 'Personal',
+            'slug'=> 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' =>'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+            'body' =>'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Eius quae totam magni repellendus error, laborum, ipsam nesciunt sunt omnis facere non.Ut recusandae et qui eos fugit. Ex mollitia id perferendis amet sint vel animi ea porro repellendus.Optio illo praesentium iure aspernatur necessitatibus non recusandae, dolores blanditiis a,excepturi eum modi molestias commodi est. Corporis, commodi quibusdam perspiciatis sitrepudiandae necessitatibus voluptatum asperiores nemo nostrum. Unde temporibus quisquam eaque soluta.',
+            'category_id'=>1,
+            'user_id' =>1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' =>'judul-Kedua',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+            'body' =>'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Eius quae totam magni repellendus error, laborum, ipsam nesciunt sunt omnis facere non.Ut recusandae et qui eos fugit. Ex mollitia id perferendis amet sint vel animi ea porro repellendus.Optio illo praesentium iure aspernatur necessitatibus non recusandae, dolores blanditiis a,excepturi eum modi molestias commodi est. Corporis, commodi quibusdam perspiciatis sitrepudiandae necessitatibus voluptatum asperiores nemo nostrum. Unde temporibus quisquam eaque soluta.',
+            'category_id'=>1,
+            'user_id' =>1
+        ]);
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-Ketiga',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+            'body' =>'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Eius quae totam magni repellendus error, laborum, ipsam nesciunt sunt omnis facere non.Ut recusandae et qui eos fugit. Ex mollitia id perferendis amet sint vel animi ea porro repellendus.Optio illo praesentium iure aspernatur necessitatibus non recusandae, dolores blanditiis a,excepturi eum modi molestias commodi est. Corporis, commodi quibusdam perspiciatis sitrepudiandae necessitatibus voluptatum asperiores nemo nostrum. Unde temporibus quisquam eaque soluta.',
+            'category_id'=>2,
+            'user_id' =>1
+        ]);
+
+        Post::create([
+            'title' => 'Judul KeEmpat',
+            'slug' => 'judul-Keempat',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit',
+            'body' =>'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Eius quae totam magni repellendus error, laborum, ipsam nesciunt sunt omnis facere non.Ut recusandae et qui eos fugit. Ex mollitia id perferendis amet sint vel animi ea porro repellendus.Optio illo praesentium iure aspernatur necessitatibus non recusandae, dolores blanditiis a,excepturi eum modi molestias commodi est. Corporis, commodi quibusdam perspiciatis sitrepudiandae necessitatibus voluptatum asperiores nemo nostrum. Unde temporibus quisquam eaque soluta.',
+            'category_id'=>2,
+            'user_id' =>2
+        ]);
+
+
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,9 +4,10 @@
 @section('container')
 <h1 class="mb-5">{{ $post->title }}</h1>
 
-<p>By.Ryan fany fadlyllah Ar in <a href="/categories/{{ $post->category->slug }}">{{$post->category->name}}</a></p>
+<p>By. <a href="#">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{$post->category->name}}</a></p>
 
 {!! $post->body !!}
 
-<a href="/posts">Back to Posts</a>
+
+<a href="/posts" class="d-block">Back To Posts</a>
 @endsection
