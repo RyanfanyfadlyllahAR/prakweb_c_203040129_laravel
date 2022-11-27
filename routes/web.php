550205f3747d5006bbae82f270a00c7f3b37be34
RyanfanyfadlyllahAR <fadlyllahfanny@gmail.com>
2022-11-27 21:40:55 +0700

    vidio 13

--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,15 @@ use App\Models\User;
 
 Route::get('/', function () {
    return view ('home', [
-    "title"=> "Home"
+    "title"=> "Home",
+    'active'=> 'home',
     ]);
 });
 
 Route::get('/about', function () {
     return view ('about', [
         "title" => "About",
+        'active'=> 'about',
         "name" => "Ryan fany fadlyllah AR",
         "email" => "fadlyllahfanny@gmail.com",
         "image" => "fadlyllah.jpg"
@@ -58,17 +60,18 @@ Route::get('categories', function(){
     ]);
 });
 
-Route::get('/categories/{category:slug}', function(Category $category){
-    return view('posts',[ 
-        'title'=> "Post By Category : $category->name",
-        'active'=> 'categories',
-        'posts'=> $category->posts->load('category', 'author')
-    ]);
-});
+// Route::get('/categories/{category:slug}', function(Category $category){
+//     return view('posts',[ 
+//         'title'=> "Post By Category : $category->name",
+//         'active'=> 'categories',
+//         'posts'=> $category->posts->load('category', 'author')
+//     ]);
+// });
 
-Route::get('/authors/{author:username}', function(User $author){
-    return view('posts',[ 
-        'title'=> "Post By Author : $author->name",
-        'posts'=> $author->posts->load('category', 'author'),
-    ]);
-});
+// Route::get('/authors/{author:username}', function(User $author){
+//     return view('posts',[ 
+//         'title'=> "Post By Author : $author->name",
+//         'active' => 'post',
+//         'posts'=> $author->posts->load('category', 'author'),
+//     ]);
+// });
