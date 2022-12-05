1b11af508cdb80396f894e53eb1b20be5b325e85
RyanfanyfadlyllahAR <fadlyllahfanny@gmail.com>
2022-12-05 20:15:59 +0700

    vidio15

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -1,27 +1,33 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class RegisterController extends Controller
 {
-   public function index(){
-    return view('register.index', [
-        'title' => 'Register',
-        'active' =>'register'
-    ]);
-   }
+    public function index() {
+        return view('register.index', [
+            'title' =>'register',
+            'active' =>'register'
+        ]);
+    }
 
-   public function store(Request $request){
-     $validatedData = $request->validate([
-        'name'=> 'required|max:225',
-        'username'=> ['required', 'min:3', 'max:225', 'unique:users'],
-        'email'=> 'required|email:dns|unique:users',
-        'password'=> 'required|min:5|max:225'
-     ]);
+    public function store(Request $request) {
 
-     User::create($validatedData);
-   }
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'username' => ['required', 'min : 3', 'max : 255', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max: 255'
+        ]);
+
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+
+        //$request->session()->flash('success', "registration success, please login");
+        return redirect('/login')->with('success', 'registration success, please login');
+    }
 }
