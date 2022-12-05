b01e9564459f09e7b48edb6afd6f013466626d68
RyanfanyfadlyllahAR <fadlyllahfanny@gmail.com>
2022-12-05 21:38:50 +0700

    vidio16

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,7 @@
+halaman dasbor
+
+
+<form action="/logout" method="post">
+    @csrf
+    <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-in-right"></i> Logout</button>
+  </form>
